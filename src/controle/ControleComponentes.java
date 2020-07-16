f7018525952e1a54e71fa22d78eb4c583b2075d0
Samuel Macedo dos Santos <samuelmacedo2011@gmail.com>
2020-07-16 17:59:02 -0300

    Estudo sobre Componentes HTML: Saídas de Texto e Imagens formatadas

---
+++ src/controle/ControleComponentes.java
@@ -0,0 +1,32 @@
+package controle;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "controleComponentes")
+@SessionScoped
+
+public class ControleComponentes implements Serializable {
+
+	private String texto;
+
+	public ControleComponentes() {
+
+	}
+
+	public String geraTexto() {
+		texto = "Texto gerado: <> & <b>TEXTO EM NEGRITO</b>";
+		return "componentesParte2";
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
